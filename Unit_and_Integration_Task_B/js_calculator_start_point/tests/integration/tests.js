98e6c5b2bffc7a5a345bbaaa89d010ce2882cb01
Pauline Rudge <paulinerudge@me.com>
2019-03-22 23:48:26 +0000

    added integration testing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -20,57 +20,56 @@ describe('calculator functionality', function() {
       running_total = element(by.css('#running_total'))
       element(by.css('#number8')).click();
       element(by.css('#operator_subtract')).click();
-      element(by.css('#number7')).click();
-      expect(running_total.getAttribute('value')).to.eventually.equal('7')
+      element(by.css('#number2')).click();
+        // element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('2')
     })
 
   it('should work with any arithmetical operator', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number4')).click();
+    element(by.css('#number7')).click();
     element(by.css('#operator_add')).click();
-    element(by.css('#number5')).click();
-    element(by.css('#operator_subtract')).click();
     element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number5')).click();
     element(by.css('#operator_multiply')).click();
     element(by.css('#number2')).click();
     element(by.css('#operator_divide')).click();
-    element(by.css('#number3')).click();
+    element(by.css('#number5')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
     it('should be able to chain multiple operators', function () {
       running_total = element(by.css('#running_total'))
-      element(by.css('#number3')).click();
+      element(by.css('#number7')).click();
       element(by.css('#operator_multiply')).click();
       element(by.css('#number9')).click();
       element(by.css('#operator_add')).click();
       element(by.css('#number2')).click();
       element(by.css('#operator_equals')).click();
-      expect(running_total.getAttribute('value')).to.eventually.equal('29')
+      expect(running_total.getAttribute('value')).to.eventually.equal('65')
     })
 
   it('should work with a range numbers(+, -, 0.1, large numbers)', function () {
     running_total = element(by.css('#running_total'))
-    element(by.css('#number3')).click();
+    element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
-    element(by.css('#number9')).click();
+    element(by.css('#number5')).click();
     element(by.css('#number0')).click();
     element(by.css('#number0')).click();
     element(by.css('#operator_divide')).click();
-    element(by.css('#number6')).click();
-    element(by.css('#number5')).click();
-    element(by.css('#number4')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
     element(by.css('#operator_subtract')).click();
     element(by.css('#number5')).click();
+    element(by.css('#number9')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('-0.8715596330275233')
-  })
-
-  // If you divide by zero, what is the effect? Infinity
+    expect(running_total.getAttribute('value')).to.eventual
 
   it('should return NaN when divided by 0', function(){
-      element(by.css('#number1')).click();
+      element(by.css('#number8')).click();
       element(by.css('#operator_divide')).click();
       element(by.css('#number0')).click();
       element(by.css('#operator_equals')).click();
