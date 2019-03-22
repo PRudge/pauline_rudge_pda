b8935d3d108b9a43ff090ec8ba324ceb957203a2
Pauline Rudge <paulinerudge@me.com>
2019-03-22 23:27:55 +0000

    testing integration

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -14,28 +14,28 @@ describe('calculator', function () {
   it('it should add two numbers together', function(){
     calculator.previousTotal = 1;
     calculator.add(4);
-    res = calculator.runningTotal
+    res = calculator.runningTotal;
     assert.equal(res, 5);
   })
 
   it('it should subtract two numbers', function(){
     calculator.previousTotal = 7;
     calculator.subtract(3);
-    res = calculator.runningTotal
+    res = calculator.runningTotal;
     assert.equal(res, 4);
   })
 
   it('it should multiply two numbers', function(){
     calculator.previousTotal = 3;
     calculator.multiply(5);
-    res = calculator.runningTotal
+    res = calculator.runningTotal;
     assert.equal(res, 15);
   })
 
   it('it should divide two numbers', function(){
     calculator.previousTotal = 21;
     calculator.divide(7);
-    res = calculator.runningTotal
+    res = calculator.runningTotal;
     assert.equal(res, 3);
   })
 
